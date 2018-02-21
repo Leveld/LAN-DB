2e63ab25d0ee1eb08ad149bd52ca186deee06eed
Jourdan Clark <jourdan_clark@hotmail.com>
2018-02-20 18:00:15 -0700

    Made ContentProducers populate ContentOutlets

--- src/server/models/ContentProducer.js
+++ src/server/models/ContentProducer.js
@@ -4,12 +4,8 @@ const UserBase = require('./bases/UserBase');
 // Add fields that only ContentProducers should have. If everyone else
 // should have the same fields, add it to UserBase instead.
 module.exports = mongoose.model('ContentProducer', UserBase({
-  contentOutlets: [
-    {
-      contentOutlet: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'ContentOutlet'
-      }
-    }
-  ]
+  contentOutlets: [ {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ContentOutlet'
+  }]
 }));
